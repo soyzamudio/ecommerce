f1e8aa1dc657a4a861a00f6b09afd00d919019cd
Jose Zamudio <zamudio@outlook.com>
2023-07-06 17:02:35 -0600

    legacy browsers

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ["localhost", "cdn.schema.io", "cdn.swell.store"],
   },
+  experimental: {
+    legacyBrowsers: false,
+  },
 };
 
 module.exports = nextConfig;
