dc245dfaa53c4469a766b1fd7099cd4ed8ab5709
Jose Zamudio <zamudio@outlook.com>
2023-07-07 10:45:02 -0600

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,9 @@ const Hero = () => {
     <section className="container mx-auto h-[635px] z-30">
       <div className="col-span-1 flex flex-col justify-center items-end gap-y-12 h-full bg-none md:bg-hero-background bg-no-repeat">
         <div className="flex flex-col text-right">
-          <h1 className="text-4xl">Cuida tu piel</h1>
-          <h1 className="text-4xl">Sin Preocupaciones.</h1>
-          <h1 className="text-4xl">Dispensary+</h1>
+          <h1 className="text-4xl md:text-7xl">Cuida tu piel</h1>
+          <h1 className="text-4xl md:text-7xl">Sin Preocupaciones.</h1>
+          <h1 className="text-4xl md:text-7xl">Dispensary+</h1>
         </div>
         <Button
           label="Mas Información"
