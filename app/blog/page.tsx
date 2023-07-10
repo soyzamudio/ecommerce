be5c4dfb9449037fd3b3dc64ceed3a4934c63231
Jose Zamudio <zamudio@outlook.com>
2023-07-10 10:42:44 -0600

    ughh

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 async function blogPosts() {
-  const fetchPosts = await fetch(`${NEXT_URL}/api/blog/posts/new`, {
+  const fetchPosts = await fetch(`${NEXT_URL}/api/blog/posts`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
